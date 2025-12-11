4b22e7212498ab9f0fa6ef154be91a7a3b0a64e5
luisD <itsolutions.luis.silva@gmail.com>
2025-12-11 16:10:35 +0000

    ATIVIDADE4

---
+++ ATIVIDADE4/script.js
@@ -0,0 +1,37 @@
+function contar() {
+    let inicio = document.getElementById('txtinicio')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpasso')
+    let res = document.getElementById('res')
+
+if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    res.innerHTML = 'Impossível contar!'
+    //window.alert('[ERRO] Faltam dados!')
+} else {
+   res.innerHTML = 'Contando: <br>'
+    let i = Number(inicio.value)
+   let f = Number(fim.value)
+   let p = Number(passo.value) 
+    if (p <= 0){
+        window.alert('Passo inválido! Considerando PASSO 1')
+        p = 1
+    }
+    if(i < f){
+        //contagem crescente
+        for(let c = i; c <= f; c += p){
+    res.innerHTML += ` ${c} \u{1F449}`
+        }
+    }else {
+        //contagem regressiva
+        for(let c = i; c >= f; c -= p){
+    res.innerHTML += ` ${c} \u{1F449}`
+        }
+   }
+    res.innerHTML += `\u{1F4A5}`
+    }
+
+}
+
+
+   
+
